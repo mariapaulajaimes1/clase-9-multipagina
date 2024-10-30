4efb0738cf64912e17a2cc8e26d7a407daae109e
mariapaulajaimes1 <167145612+mariapaulajaimes1@users.noreply.github.com>
2024-10-29 19:19:27 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,2 +1,19 @@
+# app.py
 import streamlit as st
-st.title ("Página intro")
+from streamlit_option_menu import option_menu
+
+# Configuración de página
+st.set_page_config(page_title="Aplicación Divertida", page_icon="🎉")
+
+# Menú de navegación en la barra lateral
+with st.sidebar:
+    seleccion = option_menu("Menú de Navegación", ["Inicio", "Introducción"],
+                            icons=['house', 'info-circle'],
+                            menu_icon="cast", default_index=0)
+
+# Lógica de navegación
+if seleccion == "Inicio":
+    import pages.inicio
+
+elif seleccion == "Introducción":
+    import pages.introduccion
